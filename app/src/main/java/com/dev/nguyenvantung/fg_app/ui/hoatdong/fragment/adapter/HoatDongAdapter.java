f88a77e609f5691a1def09e53723481c67ac2da9
tung.nguyenvan <nguyentungtungtungnguyen@gmail.com>
2018-11-09 13:47:26 +0700

    update interface itemHoatDong

--- app/src/main/java/com/dev/nguyenvantung/fg_app/ui/hoatdong/fragment/adapter/HoatDongAdapter.java
+++ app/src/main/java/com/dev/nguyenvantung/fg_app/ui/hoatdong/fragment/adapter/HoatDongAdapter.java
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.dev.nguyenvantung.fg_app.R;
@@ -31,15 +31,18 @@ public class HoatDongAdapter extends RecyclerView.Adapter<HoatDongAdapter.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.item_hoatdong_txt_name)
         TextView txt_name;
+     /*
         @BindView(R.id.item_hoatdong_txt_desc)
         TextView txt_desc;
+
         @BindView(R.id.item_hoatdong_txt_from_date)
         TextView txt_from_date;
+
         @BindView(R.id.item_hoatdong_txt_to_date)
         TextView txt_to_date;
+        */
         @BindView(R.id.item_hoatdong_layout)
-        LinearLayout layout;
-
+        public RelativeLayout layout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
@@ -78,11 +81,11 @@ public class HoatDongAdapter extends RecyclerView.Adapter<HoatDongAdapter.ViewHo
         HoatDong hoatDong = listHoatDong.get(i);
         String strDate = fm.format(hoatDong.getFromDate());
         viewHolder.txt_name.setText(hoatDong.getName());
-        viewHolder.txt_desc.setText(hoatDong.getDesc());
-        DateHelper.date date = DateHelper.getInstance();
-        date = dateHelper.splitDate(strDate);
-        viewHolder.txt_from_date.setText(date.getDay() + "/" + date.getMonth());
-        viewHolder.txt_to_date.setText(this.fm.format(hoatDong.getEndDate()));
+//        viewHolder.txt_desc.setText(hoatDong.getDesc());
+//        DateHelper.date date = DateHelper.getInstance();
+//        date = dateHelper.splitDate(strDate);
+//        viewHolder.txt_from_date.setText(date.getDay() + "/" + date.getMonth());
+//        viewHolder.txt_to_date.setText(this.fm.format(hoatDong.getEndDate()));
     }
 
     @Override
